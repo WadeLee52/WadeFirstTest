8d72bf20a7809cdd9e0baa7bf4b45c4fe7b49150
wade <w24810@gmail.com>
2025-07-02 14:14:08 +0800

    安裝一堆套件

--- build/lib/crawler_wadefirsttest/tasks.py
+++ build/lib/crawler_wadefirsttest/tasks.py
@@ -1,9 +1,9 @@
-from crawler.worker import app
+from crawler_wadefirsttest.worker import app
 
 
 # 註冊 task, 有註冊的 task 才可以變成任務發送給 rabbitmq
 @app.task()
 def crawler(x):
-    print("crawler")
+    print("crawler_wadefirsttest")
     print("upload db")
     return x
--- build/lib/crawler_wadefirsttest/worker.py
+++ build/lib/crawler_wadefirsttest/worker.py
@@ -1,6 +1,6 @@
 from celery import Celery
 
-from crawler.config import (
+from crawler_wadefirsttest.config import (
     RABBITMQ_HOST,
     RABBITMQ_PORT,
     WORKER_ACCOUNT,
@@ -11,9 +11,9 @@ app = Celery(
     "task",
     # 只包含 tasks.py 裡面的程式, 才會成功執行
     include=[
-        "crawler.tasks",
-        "crawler.tasks_crawler_finmind",
-        "crawler.tasks_crawler_finmind_duplicate",
+        "crawler_wadefirsttest.tasks",
+        "crawler_wadefirsttest.tasks_crawler_finmind",
+        "crawler_wadefirsttest.tasks_crawler_finmind_duplicate",
     ],
     # 連線到 rabbitmq,
     # pyamqp://user:password@127.0.0.1:5672/
